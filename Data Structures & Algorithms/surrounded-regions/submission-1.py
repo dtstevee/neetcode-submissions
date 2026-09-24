66507cbaab72c5b26abe8a8a7293f707d18c3b74
Steve Wang <gtmdqyd2@gmail.com>
2026-09-23 22:38:55 -0700

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        from collections import deque
+        rows,cols = len(board),len(board[0])
+        queue = deque()
+
+        for ri in range(rows):
+            if board[ri][0] == 'O':
+                queue.append((ri,0))
+            if board[ri][cols - 1] == 'O':
+                queue.append((ri, cols - 1))
+        for ci in range(cols):
+            if board[0][ci] == 'O':
+                queue.append((0,ci))
+            if board[rows - 1][ci] == 'O':
+                queue.append((rows-1, ci))
+
+        while queue:
+            r,c = queue.popleft()
+            board[r][c] = 'T'
+            direction = [(1,0), (-1,0), (0,1), (0,-1)]
+            for dr,dc in direction:
+                nr = r + dr
+                nc = c + dc
+                
+                if (
+                    0 <= nr < rows
+                    and 0 <= nc < cols
+                    and board[nr][nc] == 'O'):
+                    board[nr][nc] = 'T'
+                    queue.append((nr,nc))
+        
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
